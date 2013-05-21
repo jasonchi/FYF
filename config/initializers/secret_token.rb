e7a6287ef8fa1722ecc89dabf52e86dfe881ea2d
jasonchi <jasonchi1985@qq.com>
2013-05-21 14:14:03 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FyF::Application.config.secret_token = '6613787233930c38c6283f57ee755c6cac8ebc3aa4dbf99ecfe789e3add168c2487286a770e95689c83e78092e63e5cb88f80d4f1b74e4c76535b50d6a7edb2b'
